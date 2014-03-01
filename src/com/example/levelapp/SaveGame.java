407c99a2de4031f64dfa2f11f122c94cc735e571
JasonTwiddler <jas_234@hotmail.co.uk>
2014-03-01 19:44:57 +0000

    jason

--- src/com/example/levelapp/SaveGame.java
+++ src/com/example/levelapp/SaveGame.java
@@ -1,9 +1,5 @@
 package com.example.levelapp;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,8 +8,6 @@ import android.content.SharedPreferences;
 
 @SuppressLint("NewApi")
 public class SaveGame {
-	public FileInputStream inputSave;
-	public FileOutputStream outputSave;
 	SharedPreferences save = null;
 	Profile profile = null;
 			
@@ -24,15 +18,14 @@ public class SaveGame {
 	
 	void readSave(){
 		//read from save, write/create About
-		About about = profile.about;
-		about.name = save.getString("Name", "");
-		about.bio = save.getString("Bio", "");
-		about.characterLevel = save.getInt("CharacterLevel", 0);
-		about.xp = save.getInt("XP", 0);
-		about.ap = save.getInt("AP", 0);
+		profile.about.name = save.getString("Name", "");
+		profile.about.bio = save.getString("Bio", "");
+		profile.about.characterLevel = save.getInt("CharacterLevel", 0);
+		profile.about.xp = save.getInt("XP", 0);
+		profile.about.ap = save.getInt("AP", 0);
 		
 		//Set achievements by getting list of done achievement names, and altering lists
-		Set<String> doneAchievs = save.getStringSet("list", new HashSet<String>());
+		Set<String> doneAchievs = save.getStringSet("doneAchievs", new HashSet<String>());
 		Achievements a = profile.chievs;
 		
 		for(int i = 0; i < a.achievs.size(); i++){
@@ -47,9 +40,18 @@ public class SaveGame {
 				a.goals.remove(a.goals.get(i));
 			}
 		}
+		
+		Set<String> skills = save.getStringSet("skills", new HashSet<String>());
+		Skills s = profile.skills;
+
+		for(String skill : skills){
+			String[] variables = skill.split(" ");
+			s.skills.add(new Skill(SkillType.valueOf(variables[0]), Integer.parseInt(variables[1])));
+		}
 	}
 	
 	void writeSave(){
+		
 		//save About class
 		save.edit().putString("Name", profile.about.name);
 		save.edit().putString("Bio", profile.about.bio);
@@ -62,6 +64,15 @@ public class SaveGame {
 		for(Achievement a : profile.chievs.doneAchievs)
 			achievements.add(a.name);
 		save.edit().putStringSet("doneAchievs", achievements);
+		
+		//save skills
+		save.edit().putInt("TotalLevel", profile.skills.totalLevel);
+		save.edit().putInt("TotalXP", profile.skills.totalXp);
+		Set<String> skills = new HashSet<String>();
+		for(Skill a : profile.skills.skills){
+			skills.add(a.name.name() +  " " + a.xp);
+		}
+		save.edit().putStringSet("skills", skills);
 		
 		save.edit().commit();
 	}
