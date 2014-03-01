994e283ae39d5d9eb3d3b023c02eb27e20ef10e5
LukaszDygon <ld13281@my.bristol.ac.uk>
2014-03-01 15:04:24 +0000

    stffgbf

--- src/com/example/levelapp/Action.java
+++ src/com/example/levelapp/Action.java
@@ -8,19 +8,12 @@ public class Action
 	public String name;
 	public int xp;
 	ArrayList<String> skills;
-	public enum difficulty
-	{
-		easy,
-		medium,
-		hard,
-		EXTREME
-	}
 	
 	public Date deadline;
 	public int repeatDays;
-	public difficulty diff;
+	public Difficulty diff;
 	
-	public Action(String name, int xp, ArrayList<String> skills, difficulty diff, Date deadline, int repeatDays)
+	public Action(String name, int xp, ArrayList<String> skills, Difficulty diff, Date deadline, int repeatDays)
 	{
 		this.name = name;
 		this.xp = xp;
