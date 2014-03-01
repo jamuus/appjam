40ec36a79c0d4f570f45884a0f73fc901f30607f
Jamus <jamessewart@gmail.com>
2014-03-01 10:33:25 +0000

    updated stuff

---
+++ src/com/example/levelapp/Skill.java
@@ -0,0 +1,10 @@
+package com.example.levelapp;
+
+public class Skill
+{
+	public String name;
+	public int xp;
+	public int level;
+	
+	
+}
---
+++ src/com/example/levelapp/Skills.java
@@ -0,0 +1,11 @@
+package com.example.levelapp;
+
+import java.util.ArrayList;
+
+public class Skills
+{
+	public ArrayList<Skill> skills;
+	public int totalLevel;
+	public int totalXp;
+
+}
